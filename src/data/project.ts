9e995d8d448b66ea2fd6963fb841e3b74a545cd1
hulaanmo <inhog.cedrick@gmail.com>
2026-04-24 14:47:27 +0800

    feat: update project details

--- src/data/project.ts
+++ src/data/project.ts
@@ -10,28 +10,42 @@ export const projects: Project[] = [
     name: "Qmsopticore",
     description:
       "Document management system with CNN-based document and signature authentication.",
-    tech: ["TypeScript", "React", "Laravel", "MySQL", "Python", "Inertia.js"],
+    tech: ["Laravel", "React", "TypeScript", "MySQL", "Python", "Inertia.js"],
+    link: "#",
+  },
+  {
+    name: "JenVentory",
+    description:
+      "Full-stack inventory, POS and debt tracking system for a small retail store.",
+    tech: [
+      "Spring Boot",
+      "React",
+      "Typescript",
+      "TanStack Query",
+      "Zod",
+      "PostgreSQL",
+    ],
     link: "#",
   },
   {
     name: "QuizCram",
     description:
       "Educational app with quiz creation, timed testing, and automated grading.",
-    tech: ["React", "Tailwind CSS", "Laravel", "MySQL", "Shadcn", "Inertia.js"],
+    tech: ["Laravel", "React", "TypeScript", "MySQL", "Inertia.js"],
     link: "#",
   },
   {
     name: "React Redux Bank",
     description:
       "Banking interface demonstrating Redux state management with deposit and loan operations.",
-    tech: ["React", "Redux", "TypeScript", "Tailwind CSS", "Shadcn"],
+    tech: ["React", "TypeScript", "Redux"],
     link: "#",
   },
   {
     name: "The Missing Professor",
     description:
       "Interactive CTF game teaching cybersecurity through progressive challenges.",
-    tech: ["React", "TypeScript", "Shadcn", "Zod", "Zustand"],
+    tech: ["React", "TypeScript", "Zod", "Zustand"],
     link: "#",
   },
 ];
