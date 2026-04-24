9d46a927285e639487de44e0ad27b4ca09ced6f6
hulaanmo <inhog.cedrick@gmail.com>
2026-04-24 14:01:19 +0800

    update layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,22 +10,27 @@ function RootComponent() {
     <div className="relative min-h-screen">
       <div className="mx-auto max-w-2xl w-full px-6 py-12 md:py-24">
         <div className="space-y-2">
-          <h1 className="text-4xl font-medium">Cedrick</h1>
-          <div className="flex flex-wrap items-center gap-4">
-            {externalLinks.map(({ label, url }) => (
-              <a
-                key={label}
-                href={url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-muted-foreground text-sm hover:text-foreground transition-colors"
-              >
-                {label}
-              </a>
-            ))}
+          <div className="flex gap-4 items-center">
+            <img src="/me.jpg" alt="Cedrick" className="size-36 shrink-0" />
+            <div className="space-y-1">
+              <h1 className="text-4xl font-medium">Cedrick</h1>
+              <div className="flex flex-wrap items-start gap-x-4 gap-y-1">
+                {externalLinks.map(({ label, url }) => (
+                  <a
+                    key={label}
+                    href={url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-muted-foreground text-sm hover:text-foreground transition-colors"
+                  >
+                    {label}
+                  </a>
+                ))}
+              </div>
+            </div>
           </div>
 
-          <div className="flex flex-wrap gap-4 pt-1">
+          <div className="flex flex-col md:flex-row md:flex-wrap gap-2 md:gap-4 pt-1">
             {navigation.map(({ path, label }) => (
               <Link
                 key={path}
