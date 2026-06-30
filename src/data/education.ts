a56ca7d662340096a1b0adc9f1b0251cd8c0fd99
hulaanmo <inhog.cedrick@gmail.com>
2026-06-30 13:26:34 +0800

    fix educ period year

--- src/data/education.ts
+++ src/data/education.ts
@@ -8,7 +8,7 @@ export const education: Education[] = [
   {
     degree: "B.S. Information Technology",
     school: "Pamantasan ng Lungsod ng Pasig",
-    period: "Sep 2023 – June 2026",
+    period: "Sep 2023 – Jun 2026",
   },
   {
     degree: "B.S. Information Technology",
