9ac3e6ba3674b86773ba44366447635b9fb7d7fa
hulaanmo <inhog.cedrick@gmail.com>
2026-06-30 13:25:41 +0800

    update education info

--- src/data/education.ts
+++ src/data/education.ts
@@ -8,11 +8,11 @@ export const education: Education[] = [
   {
     degree: "B.S. Information Technology",
     school: "Pamantasan ng Lungsod ng Pasig",
-    period: "2023 – 2026",
+    period: "Sep 2023 – June 2026",
   },
   {
     degree: "B.S. Information Technology",
     school: "Jose Rizal University",
-    period: "2023 – 2023",
+    period: "Aug 2023 – May 2023",
   },
 ];
